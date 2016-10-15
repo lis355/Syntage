948835181609b876bea17a97286db6780bd20c94
Lartsov Ivan <lartsov@gmial.com>
2016-10-15 08:27:10 +0400

    ButterworthFilter coefficient fixes

--- Syntage/Logic/ButterworthFilter.cs
+++ Syntage/Logic/ButterworthFilter.cs
@@ -18,24 +18,23 @@ namespace Syntage.Logic
     {
         private class BiquadConvolutionTable
         {
-            public double A0;
-            public double A1;
-            public double A2;
-            public double B1;
-            public double B2;
+            public double B0, B1, B2, A1, A2;
 
             private readonly double[] _x = new double[3];
             private readonly double[] _y = new double[3];
-            
+
             public double Process(double s)
             {
+                // "сдвигаем" предыдущие семплы
                 _x[2] = _x[1];
                 _x[1] = _x[0];
                 _x[0] = s;
 
                 _y[2] = _y[1];
                 _y[1] = _y[0];
-                _y[0] = A0 * _x[0] + A1 * _x[1] + A2 * _x[2] - B1 * _y[1] - B2 * _y[2];
+
+                // свертка
+                _y[0] = B0 * _x[0] + B1 * _x[1] + B2 * _x[2] - A1 * _y[1] - A2 * _y[2];
 
                 return _y[0];
             }
@@ -65,7 +64,7 @@ namespace Syntage.Logic
         {
             if (FilterType.Value == EFilterPass.None)
                 return;
-            
+
             var count = Processor.CurrentStreamLenght;
             var lc = stream.Channels[0];
             var rc = stream.Channels[1];
@@ -89,54 +88,73 @@ namespace Syntage.Logic
 
         private void CalculateCoefficients(double cutoff)
         {
-            double a0, a1, a2, b0, b1, b2;
-            
-            var filterType = FilterType.Value;
-            if (filterType == EFilterPass.BandPass)
+            double b0, b1, b2, a0, a1, a2;
+
+            switch (FilterType.Value)
             {
-                var w = cutoff;
-                var d = w / 4;
-                var w1 = Math.Max(w - d, CutoffFrequency.Min);
-                var w2 = Math.Min(w + d, CutoffFrequency.Max);
-                w1 = TransformFrequency(w1);
-                w2 = TransformFrequency(w2);
-                var w0Sqr = w2 * w1;
-                var wd = w2 - w1;
-                
-                b0 = -1 - wd - w0Sqr;
-                b1 = (2 - 2 * w0Sqr) / b0;
-                b2 = (-1 + wd - w0Sqr) / b0;
-                a0 = -wd / b0;
-                a1 = 0;
-                a2 = wd / b0;
-            }
-            else
-            {
-                var w = TransformFrequency(cutoff);
-
-                b0 = 1 + Math.Sqrt(2) * w + w * w;
-                b1 = (-2 + 2 * w * w) / b0;
-                b2 = (1 - Math.Sqrt(2) * w + w * w) / b0;
-
-                if (filterType == EFilterPass.LowPass)
+                case EFilterPass.LowPass:
                 {
-                    a0 = w * w / b0;
-                    a1 = 2 * w * w / b0;
-                    a2 = w * w / b0;
+                    var w = TransformFrequency(cutoff);
+
+                    a0 = 1 + Math.Sqrt(2) * w + w * w;
+                    a1 = -2 + 2 * w * w;
+                    a2 = 1 - Math.Sqrt(2) * w + w * w;
+
+                    b0 = w * w;
+                    b1 = 2 * w * w;
+                    b2 = w * w;
                 }
-                else // EFilterPass.HiPass
+
+                    break;
+
+                case EFilterPass.HiPass:
                 {
-                    a0 = 1 / b0;
-                    a1 = -2 / b0;
-                    a2 = 1 / b0;
+                    var w = TransformFrequency(cutoff);
+
+                    a0 = 1 + Math.Sqrt(2) * w + w * w;
+                    a1 = -2 + 2 * w * w;
+                    a2 = 1 - Math.Sqrt(2) * w + w * w;
+
+                    b0 = 1;
+                    b1 = -2;
+                    b2 = 1;
                 }
+
+                    break;
+
+                case EFilterPass.BandPass:
+                {
+                    var w = cutoff;
+                    var d = w / 4;
+
+                    // определим полосу фильтра как [w * 3 / 4, w * 5 / 4]
+                    var w1 = Math.Max(w - d, CutoffFrequency.Min);
+                    var w2 = Math.Min(w + d, CutoffFrequency.Max);
+                    w1 = TransformFrequency(w1);
+                    w2 = TransformFrequency(w2);
+
+                    var w0Sqr = w2 * w1; // w0^2
+                    var wd = w2 - w1; // W
+
+                    a0 = -1 - wd - w0Sqr;
+                    a1 = 2 - 2 * w0Sqr;
+                    a2 = -1 + wd - w0Sqr;
+                    b0 = -wd;
+                    b1 = 0;
+                    b2 = wd;
+                }
+
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
 
-            _tablel.A0 = _tabler.A0 = a0;
-            _tablel.A1 = _tabler.A1 = a1;
-            _tablel.A2 = _tabler.A2 = a2;
-            _tablel.B1 = _tabler.B1 = b1;
-            _tablel.B2 = _tabler.B2 = b2;
+            _tablel.B0 = _tabler.B0 = b0 / a0;
+            _tablel.B1 = _tabler.B1 = b1 / a0;
+            _tablel.B2 = _tabler.B2 = b2 / a0;
+            _tablel.A1 = _tabler.A1 = a1 / a0;
+            _tablel.A2 = _tabler.A2 = a2 / a0;
         }
     }
 }
